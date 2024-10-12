51ef7d3f50b0238356222d11c878cf02a1949162
sdiazam <145729577+sdiazam@users.noreply.github.com>
2024-10-12 15:42:29 -0400

    Adding all

---
+++ SDA-ASP-Project/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using SDA_ASP_Project.Models;
+using System.Diagnostics;
+
+namespace SDA_ASP_Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SDA-ASP-Project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SDA_ASP_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
